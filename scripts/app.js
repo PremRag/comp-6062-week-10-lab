cd976f5b030f854e80ea44e38b50075eb12f4688
PremRag <Prem143rag0@gmail.com>
2025-03-13 19:24:30 -0400

    update2

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,26 @@
-const app = VTTCue.createApp({
-
-
-data() {
-    return {
-        firstName: "Prem Rag",
-        lastName: "Gudishala",
-        quantity: 0,
-        pricePerItem: 10
-    };
-},
-Computed: {
-    fullName() {
-        return `${firstName} ${lastName}`;
+const app = Vue.createApp ({
+    data() {
+        return {
+            firstName: "",
+            lastName: "",
+            quantity: 0,
+            pricePerItem: 10
+        };
     },
-    totalPrice() {
-        return `${pricePerItem} * ${quantity}`;
+    computed: {
+        fullName() {
+            return `${this.firstName} ${this.lastName}`;
+        },
+        totalPrice() {
+            return this.quantity * this.pricePerItem;
+        }
+    },
+    methods: {
+        clearField() {
+            this.firstName = "";
+            this.lastName = "";
+            this.quantity = 0;
+        }
     }
-},
-Method: {
-    clearField() {
-        this.firstName = 0,
-        this.lastName = 0,
-        this.quantity = 0
-    }
-}
 });
-app.mount("#app");
+app.mount('#app');
